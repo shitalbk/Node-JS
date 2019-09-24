8bbe8fa5608d4cd42f117ad521cd311f176fc942
shitalbk <shitalbk36@gmail.com>
2019-09-24 10:42:23 -0400

    Added new folder

---
+++ express-website/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+var app = express();
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','jade');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',function(req,res){
+   res.render('index'); 
+});
+
+app.listen(3000);
+console.log('Server is running in port 3000...');
